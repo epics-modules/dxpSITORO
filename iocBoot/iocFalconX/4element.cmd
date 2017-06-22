bf794c6262a0c35b72c6d10e3b7c6f531e2d2d06
Mark Rivers <rivers@cars.uchicago.edu>
2017-06-22 17:49:28 -0500

    Changes to load commonPlugins.cmd; previously only a few file plugins were being loaded

--- iocBoot/iocFalconX/4element.cmd
+++ iocBoot/iocFalconX/4element.cmd
@@ -2,11 +2,29 @@
 
 # Tell EPICS all about the record types, device-support modules, drivers,
 # etc. in this build from dxpApp
-dbLoadDatabase("$(DXP_SITORO)/dbd/dxp.dbd")
-dxp_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("$(DXP_SITORO)/dbd/dxpApp.dbd")
+dxpApp_registerRecordDeviceDriver(pdbbase)
 
+# Prefix for all records
+epicsEnvSet("PREFIX", "dxpSITORO:")
+# The port name for the detector
+epicsEnvSet("PORT",   "DXP1")
+# The queue size for all plugins
+epicsEnvSet("QSIZE",  "20")
+# The maximum image width; used to set the maximum size for this driver and for row profiles in the NDPluginStats plugin
+epicsEnvSet("XSIZE",  "2048")
+# The maximum image height; used to set the maximum size for this driver and for column profiles in the NDPluginStats plugin
+epicsEnvSet("YSIZE",  "2048")
+# The maximum number of time series points in the NDPluginStats plugin
+epicsEnvSet("NCHANS", "2048")
+# The maximum number of frames buffered in the NDPluginCircularBuff plugin
+epicsEnvSet("CBUFFS", "500")
+# The maximum number of threads for plugins which can run in multiple threads
+epicsEnvSet("MAX_THREADS", "8")
 # The search path for database files
 epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
+
+asynSetMinTimerPeriod(0.001)
 
 # The default callback queue in EPICS base is only 2000 bytes. 
 # The dxp detector system needs this to be larger to avoid the error message: 
@@ -16,8 +34,8 @@ callbackSetQueueSize(4000)
 
 # Setup for save_restore
 < ../save_restore.cmd
-save_restoreSet_status_prefix("dxpSITOR0:")
-dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=dxpSITORO:")
+save_restoreSet_status_prefix("$(PREFIX)")
+dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=$(PREFIX)")
 set_pass0_restoreFile("auto_settings4.sav")
 set_pass1_restoreFile("auto_settings4.sav")
 
@@ -27,40 +45,24 @@ xiaInit("falconxn4.ini")
 xiaStartSystem
 
 # DXPConfig(serverName, ndetectors, maxBuffers, maxMemory)
-NDDxpConfig("DXP1",  4, -1, -1)
+NDDxpConfig($(PORT),  4, 0, 0)
 
 dbLoadTemplate("4element.substitutions")
 
-# Create a netCDF file saving plugin
-NDFileNetCDFConfigure("DXP1NetCDF", 100, 0, "DXP1", 0)
-dbLoadRecords("$(ADCORE)/db/NDFileNetCDF.template","P=dxpSITORO:,R=netCDF1:,PORT=DXP1NetCDF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
-
-# Create a TIFF file saving plugin
-NDFileTIFFConfigure("DXP1TIFF", 20, 0, "DXP1", 0)
-dbLoadRecords("$(ADCORE)/db/NDFileTIFF.template",  "P=dxpSITORO:,R=TIFF1:,PORT=DXP1TIFF,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
-
-# Create a NeXus file saving plugin
-NDFileNexusConfigure("DXP1Nexus", 20, 0, "DXP1", 0)
-dbLoadRecords("$(ADCORE)/db/NDFileNexus.template", "P=dxpSITORO:,R=Nexus1:,PORT=DXP1Nexus,ADDR=0,TIMEOUT=1,NDARRAY_PORT=DXP1")
-
+< $(ADCORE)/iocBoot/commonPlugins.cmd
 
 #xiaSetLogLevel(4)
-#asynSetTraceMask DXP1 0 0x11
-#asynSetTraceIOMask DXP1 0 2
-
-### Scan-support software
-# crate-resident scan.  This executes 1D, 2D, 3D, and 4D scans, and caches
-# 1D data, but it doesn't store anything to disk.  (See 'saveData' below for that.)
-dbLoadRecords("$(SSCAN)/sscanApp/Db/scan.db","P=dxpSITORO:,MAXPTS1=2000,MAXPTS2=1000,MAXPTS3=10,MAXPTS4=10,MAXPTSH=2048")
+#asynSetTraceMask $(PORT) 0 0x11
+#asynSetTraceIOMask $(PORT) 0 2
 
 iocInit
 
-seq dxpMED, "P=dxpSITORO:, DXP=dxp, MCA=mca, N_DETECTORS=4, N_SCAS=4"
+seq dxpMED, "P=$(PREFIX), DXP=dxp, MCA=mca, N_DETECTORS=4, N_SCAS=4"
 
 ### Start up the autosave task and tell it what to do.
 # Save settings every thirty seconds
-create_monitor_set("auto_settings4.req", 30, "P=dxpSITORO:")
+create_monitor_set("auto_settings4.req", 30, "P=$(PREFIX)")
 
 ### Start the saveData task.
-saveData_Init("saveData.req", "P=dxpSITORO:")
+saveData_Init("saveData.req", "P=$(PREFIX)")
 
